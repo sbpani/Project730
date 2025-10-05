59f6aeb7e83ea68bb45edf725751713fb306644a
Satya <satyabrata.pani11@gmail.com>
2025-10-05 17:44:18 +0530

    updated app.py file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Jenkins Tutorials<h1/>"
+    return "<h1>Welcome to AWS Tutorials<h1/>"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0'
